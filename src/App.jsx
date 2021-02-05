64a842da85444d652c680b063be09d0108fa7b2a
hector4213 <hectorpwns@gmail.com>
2021-02-05 15:25:09 -0500

    do lets get started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import { useState } from 'react'
+import StartButton from './components/StartButton'
+import HomePage from './HomePage'
+import Container from '@material-ui/core/Container'
 
-function App() {
+const App = () => {
+	const [start, setStart] = useState(null)
+
+	const startApp = () => {
+		setStart(true)
+	}
+
+	if (start) {
+		return <HomePage />
+	}
+
 	return (
 		<div className='App'>
-			<h1>Lets learn Js</h1>
+			<Container>
+				<StartButton text={'Lets get started'} onClick={startApp} />
+			</Container>
 		</div>
 	)
 }
