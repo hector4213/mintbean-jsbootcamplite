764a92cffb5f43566e8873ef8e77fa8f675ecfe1
hector4213 <hectorpwns@gmail.com>
2021-02-06 21:41:19 -0500

    add place holder text for homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import Container from '@material-ui/core/Container'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
+import { Container } from '@material-ui/core'
 
 const HomePage = () => {
-	const [menuOpen, setMenuOpen] = useState(true)
-
-	const makeMenuItems = () => {
-		return (
-			<List>
-				<Divider />
-				{['Home', 'Arrays', 'Functions'].map((item) => (
-					<ListItem button key={item}>
-						<ListItemText primary={item} />
-					</ListItem>
-				))}
-			</List>
-		)
-	}
-
 	return (
-		<Container maxWidth='lg'>
-			<>
-				<Drawer anchor={'left'} open={menuOpen} onClose={null}>
-					{makeMenuItems()}
-				</Drawer>
-			</>
+		<Container>
+			<h1>Welcome to the homepage</h1>
+			<p>
+				Welcome to Kody! Today we are going to learn some basic JavaScript
+				fundamentals. Click on any of the lesson on the right to get started.
+				Try and complete lessons to 100%, to get some basic knowledge of
+				JavaScript.
+			</p>
 		</Container>
 	)
 }
