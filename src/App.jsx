36bb305c052c0ac3383fba72a19dd8949457fce1
hector4213 <hectorpwns@gmail.com>
2021-02-06 21:40:55 -0500

    implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,66 @@
 import './App.css'
 import { useState } from 'react'
 import StartButton from './components/StartButton'
+import Box from '@material-ui/core/Box'
+import Layout from './components/Layout'
 import HomePage from './HomePage'
-import Container from '@material-ui/core/Container'
+import VariableLesson from './lessons/VariableLesson'
+import FunctionsLesson from './lessons/FunctionsLesson'
+import ArraysLesson from './lessons/ArraysLesson'
+import { makeStyles } from '@material-ui/core/styles'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100vw',
+	},
+	start: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: '100vh',
+	},
+}))
 
 const App = () => {
-	const [start, setStart] = useState(null)
+	const classes = useStyles()
+	const [started, setStarted] = useState(false)
 
 	const startApp = () => {
-		setStart(true)
+		setStarted(true)
 	}
-
-	if (start) {
-		return <HomePage />
+	//drawers will be the links that change the page in layout
+	if (started) {
+		return (
+			<Router>
+				<div className={classes.root}>
+					<Switch>
+						<Layout>
+							<Route exact path='/'>
+								<HomePage />
+							</Route>
+							<Route path='/variables'>
+								<VariableLesson />
+							</Route>
+							<Route path='/functions'>
+								<FunctionsLesson />
+							</Route>
+							<Route path='/Arrays'>
+								<ArraysLesson />
+							</Route>
+						</Layout>
+					</Switch>
+				</div>
+			</Router>
+		)
 	}
 
 	return (
-		<div className='App'>
-			<Container>
+		<div className={classes.start}>
+			<Box>
+				<h1>Ready to learn some JS? Click below and</h1>
 				<StartButton text={'Lets get started'} onClick={startApp} />
-			</Container>
+			</Box>
 		</div>
 	)
 }
