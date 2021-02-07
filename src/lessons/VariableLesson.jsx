40c3cf9fd53937cf2f1944196e5eed0b56af1872
hector4213 <hectorpwns@gmail.com>
2021-02-06 22:33:25 -0500

    downgrade to react 16.10

--- src/lessons/VariableLesson.jsx
+++ src/lessons/VariableLesson.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Container } from '@material-ui/core'
 import { TextField } from '@material-ui/core'
 
 const VariableLesson = () => {
-	const [code, setCode] = useState('')
+	const [code, setCode] = useState('hello world')
+
+	const onCodeChange = (e) => {
+		setCode(e.target.value)
+	}
 
 	return (
 		<Container>
 			<h1>I'm the variables lesson</h1>
-			<TextField placeholder='Try your code out here!' />
 		</Container>
 	)
 }
