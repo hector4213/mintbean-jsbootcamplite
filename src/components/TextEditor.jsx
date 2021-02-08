62090269ea345da78f11102bfbce809b56b5d7ac
hector4213 <hectorpwns@gmail.com>
2021-02-08 14:41:40 -0500

    add text editor props

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 import { Controlled as CodeMirror } from 'react-codemirror2'
+import BaseBtn from './BaseBtn'
+import { makeStyles } from '@material-ui/core/styles'
 require('codemirror/mode/javascript/javascript')
 
-const TextEditor = ({ code, setCode, isReadOnly, onClick }) => {
+const useStyles = makeStyles((theme) => ({
+	root: {
+		padding: '10px',
+		display: 'flex',
+		flexDirection: 'column',
+	},
+}))
+
+const TextEditor = ({ code, setCode, isReadOnly, checkCodeAnswer }) => {
+	const classes = useStyles()
+	const onCodeChange = (userCode) => {
+		setCode(userCode)
+		console.log(userCode)
+	}
 	return (
-		<>
-			<button onClick={onClick}>Run</button>
+		<div className={classes.root}>
 			<CodeMirror
 				value={code}
 				options={{
 					mode: 'javascript',
 					theme: 'material',
 					lineNumbers: true,
+					readOnly: isReadOnly,
 				}}
 				onBeforeChange={(editor, data, value) => {
-					setCode(value)
+					onCodeChange(value)
 				}}
 				onChange={(editor, data, value) => {
-					setCode(value)
+					onCodeChange(value)
 				}}
 			/>
-		</>
+			{typeof isReadOnly !== 'string' ? (
+				<BaseBtn onClick={() => checkCodeAnswer(code)} text={'Run Code'} />
+			) : null}
+		</div>
 	)
 }
 
