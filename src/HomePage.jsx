3091e60eeaa6f02daebd8e6117925c7b3e0357ee
hector4213 <hectorpwns@gmail.com>
2021-02-08 14:52:10 -0500

    use new components

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
-
+import Header from './components/Header'
+import InfoText from './components/InfoText'
 const HomePage = () => {
 	return (
 		<Container>
-			<h1>Welcome to the homepage</h1>
-			<p>
+			<Header variant={'h3'} align={'left'}>
+				Welcome to the homepage
+			</Header>
+			<InfoText variant={'body1'}>
 				Welcome to Kody! Today we are going to learn some basic JavaScript
 				fundamentals. Click on any of the lesson on the right to get started.
 				Try and complete lessons to 100%, to get some basic knowledge of
 				JavaScript.
-			</p>
+			</InfoText>
 		</Container>
 	)
 }
