b91e9f9ed5fc8329615f81a69336cb9d691e3604
hector4213 <hectorpwns@gmail.com>
2021-02-08 14:47:13 -0500

    clean up import from components

--- src/lessons/VariableLesson.jsx
+++ src/lessons/VariableLesson.jsx
@@ -1,31 +1,14 @@
 import React, { useState } from 'react'
-import { Container, Grid } from '@material-ui/core'
-import TextEditor from '../components/TextEditor'
+import { Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import Header from '../components/Header'
-import InfoText from '../components/InfoText'
-import BaseBtn from '../components/BaseBtn'
-import { ReactComponent as VariableExample } from '../assets/variable.svg'
 import VariableTheory from './VariableTheory'
 import VariableAction from './VariableAction'
-
-const EXAMPLE =
-	'// This is an example here\nlet myCat = "fluffy"\nlet myCountry = "Canada"\nlet myRhyme = "It was all a dream, I used to read Word Up! magazine"'
-
-const useStyles = makeStyles((theme) => ({
-	gridContainer: {
-		flexGrow: 1,
-	},
-	paper: {
-		padding: '10px',
-	},
-}))
 
 const VariableLesson = ({ user }) => {
 	const [code, setCode] = useState('')
 	const [output, setOutput] = useState('')
 	const [lesson, setLesson] = useState('theory') // theory, do
-	const classes = useStyles()
 
 	const goToAction = () => {
 		setLesson('action')
