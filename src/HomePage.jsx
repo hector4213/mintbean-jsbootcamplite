e78ba023c7e1b688e98fc990df9a58f46c804a6f
hector4213 <hectorpwns@gmail.com>
2021-02-08 15:00:36 -0500

    use component in homepage add username

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { Container } from '@material-ui/core'
 import Header from './components/Header'
 import InfoText from './components/InfoText'
-const HomePage = () => {
+const HomePage = ({ user }) => {
 	return (
 		<Container>
 			<Header variant={'h3'} align={'left'}>
-				Welcome to the homepage
+				Welcome to the Kody
 			</Header>
 			<InfoText variant={'body1'}>
-				Welcome to Kody! Today we are going to learn some basic JavaScript
-				fundamentals. Click on any of the lesson on the right to get started.
-				Try and complete lessons to 100%, to get some basic knowledge of
-				JavaScript.
+				Welcome to Kody {user}! Kody is a very helpful application to help new
+				programmers learn the fundamentals of JavaScript, one of the most used
+				programming language. we Today we are going to learn some basic
+				JavaScript fundamentals. Click on any of the lesson on the right to get
+				started. Try and complete lessons to 100%, to get some basic knowledge
+				of JavaScript.
 			</InfoText>
 		</Container>
 	)
