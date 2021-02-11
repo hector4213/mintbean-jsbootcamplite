50e975ea3091d0f613f74bf0504d2c6ac21b5b82
hector4213 <hectorpwns@gmail.com>
2021-02-10 23:37:54 -0500

    add more theory, another image

--- src/lessons/VariableTheory.jsx
+++ src/lessons/VariableTheory.jsx
@@ -5,10 +5,14 @@ import Header from '../components/Header'
 import InfoText from '../components/InfoText'
 import BaseBtn from '../components/BaseBtn'
 import { ReactComponent as VariableExample } from '../assets/variable.svg'
+import AssignExample from '../assets/varexample.png'
 
 const useStyles = makeStyles((theme) => ({
 	gridContainer: {
 		flexGrow: 1,
+	},
+	example: {
+		maxWidth: '500px',
 	},
 }))
 
@@ -16,15 +20,23 @@ const VariableTheory = ({ onClick }) => {
 	const classes = useStyles()
 	return (
 		<>
-			<Header variant={'h6'}>What is a variable?</Header>
+			<Header variant={'h6'} color={'primary'}>
+				What is a variable?
+			</Header>
 			<Grid container className={classes.gridContainer} spacing={3}>
 				<Grid item xs={12} md={6} lg={6}>
 					<InfoText variant={'body1'} align={'left'}>
 						{' '}
-						A variable is a “named storage” for data. We can use variables to
-						store strings ( text ), numbers, and other data. Take a look at the
-						example, it should help visualize what a variable actually is.
-						Remember, a variable holds onto the data kind of like a box.
+						The item inside the box can be information such as username,
+						password, date of birth, or maybe a greeting. After you label the
+						boxes, you store them in the closet so when you want to return to
+						them, you can get them by their label names. The closet can be seen
+						as memory in the computer and the boxes are the variable names where
+						the items you assigned the box to can be stored. With these variable
+						names, you can store and update the information assigned to these
+						variables and also retrieve information. There are three ways (but
+						two of them are becoming more common) to create variables and that
+						is with var, let, and const. For this lesson we will only use let.
 					</InfoText>
 				</Grid>
 				<Grid
@@ -37,6 +49,27 @@ const VariableTheory = ({ onClick }) => {
 					alignItems='center'
 				>
 					<VariableExample />
+				</Grid>
+				<Grid item xs={12} md={6} lg={6}>
+					<Header variant={'h6'} color={'primary'}>
+						More about variables
+					</Header>
+					<InfoText variant={'body1'} align={'left'}>
+						The variable name goes after the variable keyword and following the
+						keyword is your variable name. You can name it whatever as long as
+						it follows naming rules (see the end of the article) and it is not a
+						reserved keyword. Reserved keywords are words that JavaScript has
+						already put aside for use for other purposes. An example is var. You
+						can’t name your variable var because that keyword is already used by
+						JavaScript declare a variable.
+					</InfoText>
+				</Grid>
+				<Grid item xs={12} md={6} lg={6}>
+					<img
+						className={classes.example}
+						src={AssignExample}
+						alt='how to assign'
+					/>
 				</Grid>
 				<Grid item xs={12}>
 					<BaseBtn text={'Try it out!'} onClick={onClick}></BaseBtn>
