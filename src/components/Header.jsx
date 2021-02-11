33817189cf49977a343189bc072141f92c9fb5e6
hector4213 <hectorpwns@gmail.com>
2021-02-11 01:38:01 -0500

    add padding to headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		paddingTop: '20px',
+		paddingBottom: '20px',
+	},
+}))
 
 const Header = ({ variant, color, align, children }) => {
+	const classes = useStyles()
 	return (
-		<Typography variant={variant} align={align} color={color}>
+		<Typography
+			className={classes.root}
+			variant={variant}
+			align={align}
+			color={color}
+		>
 			{children}
 		</Typography>
 	)
